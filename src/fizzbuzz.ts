7d5ccc589bee499e81cc08255be37e44d6a0974e
GautierL <gautier.laurent4@gmail.com>
2022-01-17 22:30:33 +0100

    refactoring phase

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export class FizzBuzz {
     play(length: number): any {
-        if(length == 3 || length == 6)
+        if(length % 3 == 0)
             return "Fizz";
         if(length == 5)
             return "Buzz";
