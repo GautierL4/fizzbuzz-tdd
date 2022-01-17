63105b55dcb63b0daff3c63dba614d307c53ce86
GautierL <gautier.laurent4@gmail.com>
2022-01-17 22:21:44 +0100

    refactor tests

--- tests/fizzbuzz.test.ts
+++ tests/fizzbuzz.test.ts
@@ -1,18 +1,21 @@
 import { FizzBuzz } from "../src/fizzbuzz"
 
 describe('Fizz Buzz Game', () => {
+    let fizzBuzz: FizzBuzz;
+
+    beforeEach(() => {
+        fizzBuzz = new FizzBuzz();
+    })
+
     it('should return first number', () => {
-        let fizzBuzz = new FizzBuzz();
         expect(fizzBuzz.play(1)).toEqual("1");
     })
 
     it('should return second number', () => {
-        let fizzBuzz = new FizzBuzz();
         expect(fizzBuzz.play(2)).toEqual("2");
     })
 
     it('should return fizz for 3', () => {
-        let fizzBuzz = new FizzBuzz();
         expect(fizzBuzz.play(3)).toEqual("Fizz");
     })
 
